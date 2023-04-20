82668744a0ba60d930e4cfd66af160e6c8b4617c
JP Appel <jeanpierre.appel01@gmail.com>
2023-04-19 23:13:04 -0400

    implemented joke command

--- dadbot.py
+++ dadbot.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 import sys
 import os
+from asyncio import sleep
+
 import discord
 import dotenv
 
 from discord.ext import commands
+from jokes import *
 
 
 dotenv.load_dotenv()
@@ -16,6 +19,8 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+jokes = Jokes("jokes.txt")
+
 @bot.event
 async def on_ready():
         print(f'{bot.user} is online')
@@ -25,6 +30,13 @@ async def on_ready():
 async def ping(ctx):
     """Responds to ping with pong"""
     await ctx.send("pong")
+
+@bot.command()
+async def joke(ctx):
+    """Tells jokes from a file"""
+    for part in jokes.tell_joke():
+        await ctx.send(part)
+        await sleep(1)
 
 if __name__ == "__main__":
     if DISCORD_TOKEN == None:
